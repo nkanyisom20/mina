b38867e38e148e76a2b57e5d25a7fc4e7e120599
nkanyisom20 <57561518+nkanyisom20@users.noreply.github.com>
2024-01-15 18:55:59 +0200

    Update connect.php

--- zona/connect.php
+++ zona/connect.php
@@ -1,1 +1,14 @@
-
+<?php
+$servername = "localhost";
+$server_user = "id19797999_lf_user";
+$server_pass = "@Nkah_123!";
+$db_name = "id19797999_lf_db";
+// db connection
+$connect = new mysqli($servername, $server_user, $server_pass, $db_name);
+// check connection
+if($connect->connect_error) {
+  die("Connection Failed : " . $connect->connect_error);
+} else {
+  // echo "Successfully connected";
+}
+?>
