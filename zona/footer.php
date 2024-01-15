1ffeeefe499e3dc554e5db78b5c8c7713084239e
nkanyisom20 <57561518+nkanyisom20@users.noreply.github.com>
2024-01-15 19:12:56 +0200

    Delete zona directory

--- zona/footer.php
+++
@@ -1,2 +0,0 @@
-<p style="color: green"><i><b>Lost & Found is the IT company located at All Universities in South Africa.</b></i></p>
-<p style="color: lightgrey"><i><b>Copyright | All Rights Reserved | Designed by Nkanyiso Consulting</b></i></p>
